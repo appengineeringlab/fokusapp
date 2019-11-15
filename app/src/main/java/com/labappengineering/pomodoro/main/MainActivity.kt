908523330d169c37af023ba7839ec286eb2beee1
App Engineering Lab <57107084+appengineeringlab@users.noreply.github.com>
2019-11-15 02:37:52 +0100

    Created bottom app bar.

--- app/src/main/java/com/labappengineering/pomodoro/main/MainActivity.kt
+++ app/src/main/java/com/labappengineering/pomodoro/main/MainActivity.kt
@@ -2,12 +2,21 @@ package com.labappengineering.pomodoro.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.labappengineering.pomodoro.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(bar)
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.bottomappbar_menu, menu)
+        return true
     }
 }
