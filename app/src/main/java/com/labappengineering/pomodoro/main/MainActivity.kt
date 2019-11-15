545ab780596c0072216e159a5fa8344d3be175de
App Engineering Lab <57107084+appengineeringlab@users.noreply.github.com>
2019-11-15 01:36:45 +0100

    Finished splash activity.

---
+++ app/src/main/java/com/labappengineering/pomodoro/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.labappengineering.pomodoro.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.labappengineering.pomodoro.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- app/src/main/java/com/labappengineering/pomodoro/splash/SplashActivity.kt
+++ app/src/main/java/com/labappengineering/pomodoro/splash/SplashActivity.kt
@@ -1,11 +1,17 @@
 package com.labappengineering.pomodoro.splash
 
+import android.app.ActivityOptions
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import com.labappengineering.pomodoro.R
+import com.labappengineering.pomodoro.main.MainActivity
 import kotlinx.android.synthetic.main.activity_splash.*
+import android.os.Build
+
+
 
 class SplashActivity : AppCompatActivity() {
 
@@ -14,6 +20,18 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
         animateText()
         animateButton()
+        splash_mb_go.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+//            if (Build.VERSION.SDK_INT > 20) {
+//                val options = ActivityOptions.makeSceneTransitionAnimation(this)
+//                startActivity(intent, options.toBundle())
+//            } else {
+//                startActivity(intent)
+//            }
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+
+        }
     }
 
     private fun animateButton() {
