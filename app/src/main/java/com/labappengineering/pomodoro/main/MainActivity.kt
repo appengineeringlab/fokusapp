17115f65c7991e3d16aae0a850eb112643d43154
App Engineering Lab <57107084+appengineeringlab@users.noreply.github.com>
2019-11-16 02:47:44 +0100

    Created main activity layout.

--- app/src/main/java/com/labappengineering/pomodoro/main/MainActivity.kt
+++ app/src/main/java/com/labappengineering/pomodoro/main/MainActivity.kt
@@ -2,12 +2,32 @@ package com.labappengineering.pomodoro.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.labappengineering.pomodoro.R
+import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.view.menu.MenuBuilder
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(main_bap)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_bap_menu, menu)
+
+        if (menu is MenuBuilder) {
+            menu.setOptionalIconsVisible(true)
+        }
+
+
+        return true
     }
 }
