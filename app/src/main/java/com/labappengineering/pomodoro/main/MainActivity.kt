dfb54d888a554c54eebe11578082edf4992beb28
App Engineering Lab <57107084+appengineeringlab@users.noreply.github.com>
2019-11-16 03:23:51 +0100

    Added card views.

--- app/src/main/java/com/labappengineering/pomodoro/main/MainActivity.kt
+++ app/src/main/java/com/labappengineering/pomodoro/main/MainActivity.kt
@@ -6,6 +6,13 @@ import android.view.Menu
 import com.labappengineering.pomodoro.R
 import kotlinx.android.synthetic.main.activity_main.*
 import androidx.appcompat.view.menu.MenuBuilder
+import android.view.animation.Animation.RELATIVE_TO_SELF
+import android.view.animation.RotateAnimation
+import android.os.CountDownTimer
+
+
+
+
 
 
 
@@ -17,6 +24,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         setSupportActionBar(main_bap)
+
+
+        val makeVertical = RotateAnimation(0f, -90f, RELATIVE_TO_SELF, 0.5f, RELATIVE_TO_SELF, 0.5f)
+        makeVertical.fillAfter = true
+        main_pb.startAnimation(makeVertical)
+        main_pb.setSecondaryProgress(50)
+        main_pb.setProgress(0)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -30,4 +44,5 @@ class MainActivity : AppCompatActivity() {
 
         return true
     }
+
 }
