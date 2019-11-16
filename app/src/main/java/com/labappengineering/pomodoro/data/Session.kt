4f45e8d4e90dd1e5e2e9ee6c75148e24f782291e
App Engineering Lab <57107084+appengineeringlab@users.noreply.github.com>
2019-11-16 19:00:14 +0100

    Created model and DAO for session.

---
+++ app/src/main/java/com/labappengineering/pomodoro/data/Session.kt
@@ -0,0 +1,19 @@
+package com.labappengineering.pomodoro.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity(tableName = "sessions")
+data class Session @JvmOverloads constructor(
+    @PrimaryKey @ColumnInfo(name="_id")
+    var id: String = UUID.randomUUID().toString(),
+
+    var length: Int = 25,
+    var shortBreak: Int = 5,
+    var longBreak: Int = 25,
+    var repetitions: Int = 4,
+    var perDay: Int = 10
+){
+}
