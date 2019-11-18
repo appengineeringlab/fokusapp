a02c92405279188871ef8024bcc07bab8033467a
appengineeringlab <57107084+appengineeringlab@users.noreply.github.com>
2019-11-18 18:30:11 +0100

    Removed nonsence if from splasviewmodel.

--- app/src/main/java/com/labappengineering/pomodoro/splash/SplashViewModel.kt
+++ app/src/main/java/com/labappengineering/pomodoro/splash/SplashViewModel.kt
@@ -5,12 +5,9 @@ import com.labappengineering.pomodoro.util.IRepository
 
 class SplashViewModel(repository: IRepository<Session>) : BaseViewModel<Session>(repository) {
     fun initializeDB(sessions : List<Session>){
-        if(sessions == null ){
+        if(sessions.isEmpty() ){
             insert(Session(length = 1, repetitions = 2, perDay = 3))
-        } else if(sessions != null && sessions.isEmpty()) {
-             insert(Session(length = 1, repetitions = 2, perDay = 3))
         }
-
     }
 
     fun clearDB() {
